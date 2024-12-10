df2ebf94632363be39de5ede86603053a7bbff06
thomas <phat.vuong@stu.ocsb.ca>
2024-12-10 15:41:55 +0000

    update

--- main.js
+++ main.js
@@ -20,3 +20,26 @@ function factorial(n) {
     return result;
 }
 alert
+
+function count_up (start, stop) { 
+    for ( let i= start; i< stop; i++) {
+        console.log(i);
+    }
+}
+count_up (1,5);
+
+function sum(n) {
+    let total = 0; // Initialize total to store the sum
+    for (let i = 1; i <= n; i++) { // Loop from 1 to n
+      total += i; // Add the current value of i to total
+    }
+    return total; 
+  }
+  
+console.log(sum(7));
+
+
+function sum_divisible (n, x) {
+    let total = 0;
+    for
+}
